c338bcd781c1b445e71803520227f9f566ee0392
Jerry Jäppinen <eiskis@gmail.com>
2024-01-22 21:14:31 +0100

    Folder and ID file creation works

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function l (...args) {
   console.log(...args)
 }
 
+// https://stackoverflow.com/questions/9705194/replace-special-characters-in-a-string-with-underscore
+// https://stackoverflow.com/questions/7635952/javascript-how-to-remove-all-extra-spacing-between-words
+function normalizeFilename (str) {
+  return str.replace(/[^a-zA-Z0-9]/g, ' ').replace(/\s+/g, " ").replace(/^\s+|\s+$/g, '')
+}
+
 // Remove existing folder
 // https://nodejs.org/en/learn/manipulating-files/working-with-folders-in-nodejs
 function clearBuildDir () {
@@ -26,15 +32,23 @@ function clearBuildDir () {
 }
 
 function createBuildDir () {
-  fs.mkdirSync(buildDir)
+  fs.mkdirSync(buildDir, { recursive: true })
   l(`Build directory created`);
+}
+
+function createDir (path) {
+  fs.mkdirSync(`${buildDir}/${path}`, { recursive: true })
+}
+
+function getSubDir (...pathSegments) {
+  return pathSegments.map(normalizeFilename).join('/')
 }
 
 function writeTextFile (path, content) {
   const fullPath = `${buildDir}/${path}`
 
   fs.writeFileSync(fullPath, content)
-  console.log("File has been saved.")
+  console.log(`File ${path} saved.`)
 }
 
 
@@ -55,6 +69,15 @@ ${rows}
 `
 }
 
+function composeIdFile (id) {
+  return id
+}
+
+function composeGameReadme (name, id) {
+  return `Put game files of ${name} (id ${id}) into this folder.`
+}
+
+
 
 // Build process
 function build () {
@@ -63,6 +86,21 @@ function build () {
 
   // Write Readme
   writeTextFile('README.txt', composeReadme())
+
+  // Each game
+  data.gameIds.forEach(([name, id]) => {
+    const dir = getSubDir(name)
+
+    // Create folder
+    createDir(dir)
+
+    // Create folder with instructions
+    writeTextFile(`${dir}/PUT GAME FILES HERE.txt`, composeGameReadme(name, id))
+
+    // Create mapper file for ScummVM
+    writeTextFile(`${dir}.scummvm`, composeIdFile(id))
+
+  })
 }
 
 // Actually run the build
